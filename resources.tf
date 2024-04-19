771d59420013307cedede630055bc5c4a311d889
dinilv <dinil@github.com>
2024-04-19 16:06:25 +0530

    fix: created terraform bucket manually

--- resources.tf
+++ resources.tf
@@ -1,11 +1,13 @@
+# Configure Google Cloud Project Data Source
 data "google_project" "existing" {
- project_id = "gcpa-415907"
+  project_id = "gcpa-415907"
 }
 
+# Artifact Registry Repository (assuming it depends on the bucket)
 resource "google_artifact_registry_repository" "docker_images" {
- project       = data.google_project.existing.project_id
- location      = "us-central1"
- repository_id = "amazon-seller-ai"
- description   = "amazon-seller-ai images"
- format        = "DOCKER"
+  project       = data.google_project.existing.project_id
+  location      = "us-central1"
+  repository_id = "amazon-seller-ai"
+  description   = "amazon-seller-ai images"
+  format        = "DOCKER"
 }
